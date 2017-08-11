b8b5f1c433af6b6fe8cd034c33a24964f9f54f0b
QQYES <qqyesqq1@126.com>
2017-08-11 18:31:29 +0800

    测试

--- MaximumBinaryTree.py
+++ MaximumBinaryTree.py
@@ -14,25 +14,25 @@ class Solution:
         # tree_node = TreeNode()
 
         if isinstance(nums, list):
+
             sort_nums = nums.copy()
             sort_nums.sort()
             # print(nums.index(sort_nums[-1]))
+            if nums.__len__() != 1:
+                max_index = nums.index(sort_nums[-1])
+                tree_node = TreeNode(max_index)
 
-            max_index = nums.index(sort_nums[-1])
-            tree_node = TreeNode(max_index)
-
-            tree_node.left = nums[0:max_index]
-            tree_node.right = nums[max_index + 1: nums.__len__()]
-            # print(tree_node.left, tree_node.right)
-            self.constructMaximumBinaryTree(tree_node.left)
-            self.constructMaximumBinaryTree(tree_node.right)
-            if tree_node.left.__len__() != 1:
-                return tree_node.left
-            if tree_node.right.__len__() != 1:
-                return tree_node.right
-
-
-
+                tree_node.left = nums[0:max_index]
+                tree_node.right = nums[max_index + 1: nums.__len__()]
+                # print(tree_node.left, tree_node.right)
+                self.constructMaximumBinaryTree(tree_node.left)
+                self.constructMaximumBinaryTree(tree_node.right)
+                if tree_node.left.__len__() != 1:
+                    return tree_node.left
+                if tree_node.right.__len__() != 1:
+                    return tree_node.right
+            else:
+                return tree_node
 
 solution = Solution()
 result = solution.constructMaximumBinaryTree([3, 2, 1, 6, 0, 5])
